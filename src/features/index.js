a06deb07165fcf6dc2b4645c4a1e36f7d570898d
sifatulrabbi <mdsifatulislam.rabbi@gmail.com>
2022-03-29 14:28:40 +0600

    feat(page): register page

--- src/features/index.js
+++ src/features/index.js
@@ -1,6 +1,7 @@
 export {default as ProtectedRoutes} from "./ProtectedRoutes";
 export {default as BottomBar} from "./BottomBar";
 export {default as LoginForm} from "./LoginForm";
+export {default as RegisterForm} from "./RegisterForm";
 export {default as Header} from "./Header";
 export {default as Loading} from "./Loading";
 export {default as Overview} from "./Overview";
